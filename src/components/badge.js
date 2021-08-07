3259c25ca68b2f6a27abe0e0396ed522fbf07d55
juanml30 <moreno.l.juan@gmail.com>
2021-08-07 20:22:11 -0300

    Agregue clases, estilos y bootstrap al proyecto

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,26 +1,28 @@
 import React from "react";
+import "./styles/Badge.css";
+
 import confLogo from "../images/badge-conf.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge_header">
           <img src={confLogo} alt="Logo" />
         </div>
-        <div>
-          <img src="" alt="Avatar" />
+        <div className="Badge_section-name">
+          <img src="" alt="Avatar" className="Badge_avatar" />
           <h1>
             Juan <br />
             Moreno
           </h1>
         </div>
-        <div>
-          <p>Frontend Developer</p>
-          <p>@juanml30</p>
+        <div className="Badge_section-info">
+          <h3>Frontend Developer</h3>
+          <div>@juanml30</div>
         </div>
 
-        <div>#Platziconf</div>
+        <div className="Badge_footer">#Platziconf</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
 import Badge from "./components/badge";
 
 const container = document.getElementById("app");
