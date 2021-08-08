eb51d15bca735dd2909c8820c4519973da3d5f67
juanml30 <moreno.l.juan@gmail.com>
2021-08-08 20:57:24 -0300

    agregue la navbar y el badget dentro de la pagina

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from "react";
 
+import "./styles/Navbar.css";
+import logo from "../images/logo.svg";
+
 class Navbar extends React.Component {
   render() {
-    return <h1>React Playing</h1>;
+    return (
+      <div className="navbar">
+        <div className="container-fluid">
+          <a className="navbar__brand" href="/">
+            <img className="navbar__logo" src={logo} alt="Logo" />
+            <span className="font-weight-light">Platzi</span>
+            <span className="font-weight-light">Conf</span>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import header from "../images/badge-conf.svg";
+import "./styles/BadgeNew.css";
+import Badge from "../components/Badge";
 
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Juan"
+                lastName="Moreno"
+                jobTitle="Front End Developer"
+                twitter="juan_ml_30"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
