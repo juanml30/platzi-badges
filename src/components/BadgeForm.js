c982a6c06a72717f4ca8c355d4594a5efe3b01cd
juanml30 <moreno.l.juan@gmail.com>
2021-08-09 07:37:18 -0300

    Genere el formulario ademas de un manejo de eventos con el button y onchange

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  handleChange = (e) => {
+    console.log({ name: e.target.name, value: e.target.value });
+  };
+  handleClick = (e) => {
+    console.log("button fue clickado papu");
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Was submited");
+  };
   render() {
     return (
       <div>
-        <h1>asdasdasd</h1>
+        <h1>New Attendant</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="">First Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="firstName"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Save
+          </button>
+        </form>
       </div>
     );
   }
