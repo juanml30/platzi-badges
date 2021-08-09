e0cac99152edcb97de3f6fd823cb194af106ad94
juanml30 <moreno.l.juan@gmail.com>
2021-08-08 21:10:23 -0300

    Solo cree el componente Form, me falta llenarlo y usarlo

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BadgeForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>asdasdasd</h1>
+      </div>
+    );
+  }
+}
+
+export default BadgeForm;
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import header from "../images/badge-conf.svg";
 import "./styles/BadgeNew.css";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
   render() {
@@ -14,13 +15,16 @@ class BadgeNew extends React.Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge
                 firstName="Juan"
                 lastName="Moreno"
                 jobTitle="Front End Developer"
                 twitter="juan_ml_30"
               />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
