dedd37cd9dd709b0a4a586ae468f2448b5af8f59
juanml30 <moreno.l.juan@gmail.com>
2021-08-11 19:59:59 -0300

    Creada la nueva pagina y algun boton

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
+//import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
